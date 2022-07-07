fd32ffa73070aa797ac9f7c94624959f873c27f8
ghost_32 <kalash.strt@gmail.com>
2022-07-07 22:12:20 +0530

    add: arrays - rotateImage

---
+++ src/Arrays/RotateImage/Approach1.java
@@ -0,0 +1,52 @@
+package Arrays.RotateImage;
+
+// Approach: Using auxiliary space
+// Time Complexity: O(n^2)
+// Space Complexity: O(n x n) for auxiliary array
+
+import java.util.*;
+public class Approach1 {
+    public static int[][] rotateImage(int[][] mat){
+        int[][] rotated = new int[mat.length][mat.length];
+
+        // transpose
+        for(int row=0; row<mat.length; row++){
+            for(int col=0; col<mat.length; col++){
+                rotated[row][col] = mat[col][row];
+            }
+        }
+
+        // reverse rows
+        for(int row=0; row<rotated.length; row++){
+            int p = 0;
+            int q = rotated.length-1;
+
+            while(p < q){
+                int temp = rotated[row][p];
+                rotated[row][p] = rotated[row][q];
+                rotated[row][q] = temp;
+
+                p++;
+                q--;
+            }
+        }
+
+        return rotated;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int[][] matrix = new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                matrix[i][j] = in.nextInt();
+            }
+        }
+
+        int[][] result = rotateImage(matrix);
+        for(int[] row : result)
+            System.out.println(Arrays.toString(row));
+    }
+}
---
+++ src/Arrays/RotateImage/Approach2.java
@@ -0,0 +1,55 @@
+package Arrays.RotateImage;
+
+// Approach:
+// Time Complexity:
+// Space Complexity:
+
+import java.lang.reflect.Array;
+import java.util.*;
+public class Approach2 {
+    public static void reverse(int[] nums){
+        int p = 0;
+        int q = nums.length-1;
+
+        while(p < q){
+            int temp = nums[p];
+            nums[p] = nums[q];
+            nums[q] = temp;
+
+            p++;
+            q--;
+        }
+    }
+
+    public static void rotate(int[][] nums){
+        // transpose
+        for(int row=0; row<nums.length; row++){
+            for(int col=row; col<nums[row].length; col++){
+                int temp = nums[row][col];
+                nums[row][col] = nums[col][row];
+                nums[col][row] = temp;
+            }
+        }
+
+        // reverse row
+        for(int[] row : nums)
+            reverse(row);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int[][] nums = new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                nums[i][j] = in.nextInt();
+            }
+        }
+
+        rotate(nums);
+
+        for(int[] row : nums)
+            System.out.println(Arrays.toString(row));
+    }
+}
