6d3efd14a46c031ddb0ffd6972785d736ec4a890
ghost_32 <kalash.strt@gmail.com>
2022-07-09 12:32:53 +0530

    add: arrays - moveAllNegativeToOneSide

---
+++ src/Arrays/MoveAllNegativesToOneSide/Approach1.java
@@ -0,0 +1,34 @@
+package Arrays.MoveAllNegativesToOneSide;
+
+// Approach:
+// Time Complexity:
+// Space Complexity:
+
+import java.util.*;
+public class Approach1 {
+    public static int[] moveElements(int[] nums){
+        int[] aux = new int[nums.length];
+        int counter = 0;
+
+        for(int num : nums)
+            if(num >= 0) aux[counter++] = num;
+
+        for(int num : nums)
+            if(num < 0) aux[counter++] = num;
+
+        return aux;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int[] nums = new int[n];
+        for(int i=0; i<n; i++)
+            nums[i] = in.nextInt();
+
+        nums = moveElements(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
